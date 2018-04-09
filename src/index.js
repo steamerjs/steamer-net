3b40f3cffeb439c6e02bbd9bade5b1999d0aad6d
lcxfs1991 <lcxfs1991@gmail.com>
2018-04-09 19:45:14 +0800

    fix ie11 timeout issue

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@
 
  if (typeof window !== "undefined") {
      global = window;
- } 
+ }
  else if (typeof self !== "undefined"){
      global = self;
  }
- 
+
  // global config for whole plugin
  let config = {
      dataReturnSuccessCondition: function() {
@@ -25,18 +25,18 @@
          successCb(data);
      }
  };
- 
+
  // readyState const
  const DONE = 4;
- 
+
  // status code
  const STATE_200 = 200;
- 
+
  // empty function
  function emptyFunc() {}
- 
+
  function makeOpts(options) {
- 
+
      options = config.beforeRequest(options);
      let opts = {};
          opts.url = options.url,
@@ -53,26 +53,26 @@
          opts.method = opts.method.toUpperCase();
      return opts;
  }
- 
- 
+
+
  /**
   * create xhr
   * @return {Object} [xhr object]
   */
  function createXHR() {
- 
+
      var xhr = null;
- 
+
      var XMLHttpFactories = [
-         
+
          function () { return new XMLHttpRequest(); },
          function () { return new XDomainRequest(); },
          function () { return new ActiveXObject("Msxml2.XMLHTTP"); },
          function () { return new ActiveXObject("Msxml3.xmlhttp"); },
          function () { return new ActiveXObject("Microsoft.XMLHTTP"); }
- 
+
      ];
- 
+
      for (var i = 0, len = XMLHttpFactories.length; i < len; i++) {
          try {
              xhr = XMLHttpFactories[i]();
@@ -82,12 +82,12 @@
          }
          break;
      }
- 
+
      return xhr;
- 
+
  }
- 
- 
+
+
  /**
   * make url/request param
   * @param  {Object} paramObj [param object passed by user]
@@ -95,14 +95,14 @@
   */
  function makeParam(paramObj) {
      let paramArray = [];
- 
+
      for(let key in paramObj){
          paramArray.push(key + '=' + encodeURIComponent(paramObj[key]));
      }
- 
+
      return  paramArray.join('&');
  }
- 
+
  /**
   * make url with param
   * @param  {String} url        [original url]
@@ -113,22 +113,22 @@
      url += (!!~url.indexOf('?') ? '&' : '?') + paramString;
      return url;
  }
- 
+
  /**
   * set request headers
-  * @param {Object} xhr 
-  * @param {Object} headers 
+  * @param {Object} xhr
+  * @param {Object} headers
   */
  function makeHeaders(xhr, headers, method) {
- 
+
      if (!xhr.setRequestHeader) {
          return;
      }
- 
+
      if ((method === 'GET' || method === 'POST')) {
          xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
      }
- 
+
      var headers = headers || {};
      for (let key in headers) {
          if (headers.hasOwnProperty(key)) {
@@ -136,7 +136,7 @@
          }
      }
  }
- 
+
  function makeXhrFields(xhr, xhrFields) {
      xhr.withCredentials = true;
 
@@ -144,144 +144,140 @@
         xhr.withCredentials = false;
      }
  }
- 
+
  export function ajaxInit(cf) {
     config.dataReturnSuccessCondition = cf.dataReturnSuccessCondition || config.dataReturnSuccessCondition;
     config.beforeRequest = cf.beforeRequest || config.beforeRequest;
     config.beforeResponse = cf.beforeResponse || config.beforeResponse;
  }
- 
+
  export function ajaxGet(options) {
      let opts = makeOpts(options),
          paramString = makeParam(opts.paramObj),
          url = makeUrl(opts.url, paramString);
- 
+
      var xhr = sendReq(opts);
- 
+
      if (!xhr) {
-         return;  
-     } 
- 
+         return;
+     }
+
      xhr.open('GET', url, true);
+     setXhrTimeout(xhr, opts); // IE11一定要在xhr.open之后设置timeout时间
      makeXhrFields(xhr, opts.xhrFields);
-     // xhr.setRequestHeader && xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
      makeHeaders(xhr, opts.headers, 'GET');
      xhr.send();
  }
- 
+
  export function ajaxPost(options) {
      let opts = makeOpts(options),
          paramString = makeParam(opts.paramObj),
          url = opts.url;
- 
+
      var xhr = sendReq(opts);
- 
-     if (!xhr) {
-         return;  
-     } 
- 
-     xhr.open('POST', url, true);
-     makeXhrFields(xhr, opts.xhrFields);
-     // xhr.setRequestHeader && xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
-     makeHeaders(xhr, opts.headers, 'POST');
-     xhr.send(paramString);
- }
- 
- function ajaxForm(options) {
-     var opts = makeOpts(options),
-         url = opts.url;
- 
-     var xhr = sendReq(opts);
- 
+
      if (!xhr) {
          return;
      }
- 
+
      xhr.open('POST', url, true);
+     setXhrTimeout(xhr, opts); // IE11一定要在xhr.open之后设置timeout时间
+     makeXhrFields(xhr, opts.xhrFields);
+     makeHeaders(xhr, opts.headers, 'POST');
+     xhr.send(paramString);
+ }
+
+ function ajaxForm(options) {
+     var opts = makeOpts(options),
+         url = opts.url;
+
+     var xhr = sendReq(opts);
+
+     if (!xhr) {
+         return;
+     }
+
+     xhr.open('POST', url, true);
+     setXhrTimeout(xhr, opts); // IE11一定要在xhr.open之后设置timeout时间
      makeXhrFields(xhr, opts.xhrFields);
      makeHeaders(xhr, opts.headers, 'FORM');
      xhr.send(opts.paramObj);
  }
- 
+
  /**
   * jsonp
   * @param  {[type]} options [description]
   * @return {[type]}         [description]
   */
  export function ajaxJsonp(options) {
- 
+
      let opts = makeOpts(options);
- 
+
      if (!opts.paramObj) {
          throw new Error("Please provide parameter for jsonp");
      }
- 
+
      if (!opts.paramObj.jsonCbName) {
          opts.paramObj.jsonCbName = "jsonCb_" + Date.now();
      }
- 
+
      opts.paramObj.callback = opts.paramObj.jsonCbName;
      delete opts.paramObj['jsonCbName'];
- 
+
      global[opts.paramObj.callback] = function(data) {
          if (opts.localData) {
              onDataReturn(opts.localData, opts);
              return;
          }
- 
+
          onDataReturn(data, opts);
          removeScript(script);
      };
- 
+
      function removeScript(st) {
          setTimeout(function() {
              st.parentNode.removeChild(st);
              st = null;
          }, 200);
      }
- 
+
      let paramString = makeParam(opts.paramObj),
          url = makeUrl(opts.url, paramString),
          script = (global && global.document) ? global.document.createElement("script") : {},
          head = (global && global.document) ? global.document.getElementsByTagName("head")[0] : {};
-     
+
      script.src = url;
      head.appendChild(script);
- 
+
      script.onerror = function(err) {
          opts.errorCb({errCode: err});
          removeScript(script);
      };
  }
- 
+
  function onDataReturn(data, opts) {
      config.beforeResponse(data, function(data) {
          let isSuccess = config.dataReturnSuccessCondition(data);
          isSuccess ? opts.successCb(data) : opts.errorCb(data);
      }, opts.errorCb);
  }
- 
+
  function sendReq(opts) {
      var xhr = createXHR();
-     
+
      if (!xhr) {
          throw new Error('XMLHttp is not defined');
      }
- 
+
      // 如果本地已经从别的地方获取到数据，就不用请求了
      if (opts.localData) {
          onDataReturn(opts.localData, opts);
          return;
      }
 
-     // 设置timeout
-     if (opts.timeout) {
-        xhr.timeout = opts.timeout;
-     }
-
      // 是否async
     xhr.async = (opts.async === false) ? false : true;
- 
+
      xhr.onreadystatechange = function() {
          if (xhr.readyState === DONE) {
              if (xhr.status === STATE_200) {
@@ -298,7 +294,7 @@
              }
          }
      };
- 
+
      xhr.onerror = function() {
          opts.errorCb({
              errCode: -1
@@ -310,14 +306,21 @@
             errCode: -2
         });
     };
- 
+
      return xhr;
  }
- 
+
+ function setXhrTimeout(xhr, opts) {
+    // 设置timeout
+    if (opts.timeout) {
+        xhr.timeout = opts.timeout;
+    }
+ }
+
  function ajax(options) {
- 
+
      let opts = makeOpts(options);
- 
+
      switch(opts.method) {
          case 'JSONP':
              ajaxJsonp(options);
@@ -332,17 +335,16 @@
              ajaxForm(options);
              break;
      }
- 
+
  }
- 
+
  let net = {
      ajax,
      ajaxGet,
      ajaxPost,
      ajaxForm,
-     ajaxJsonp,  
-     ajaxInit, 
+     ajaxJsonp,
+     ajaxInit,
  };
- 
+
  export default net;
- 
